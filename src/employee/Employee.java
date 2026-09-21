b9ffa0c286cec63eb9da6235005b8baef5f69797
Piyush <156587759+piko4@users.noreply.github.com>
2026-09-21 14:18:47 +0530

    new

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -1,5 +1,13 @@
 package employee;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -40,10 +48,10 @@ class Manager extends Employee
 {
     public int hrqScore;
 
-    public Manager(String name, String role, String address, String gender, double basicSalary, int age, int hrqScore)
+    public Manager(String name, String address, String gender, double basicSalary, int age, int hrqScore)
     {
 	this.name = name;
-	this.role = role;
+	this.role = "Manager";
 	this.address = address;
 	this.gender = gender;
 	this.basicSalary = basicSalary;
@@ -58,10 +66,10 @@ class Engineer extends Employee
 {
     int overtime;
 
-    public Engineer(String name, String role, String address, String gender, double basicSalary, int age, int overtime)
+    public Engineer(String name, String address, String gender, double basicSalary, int age, int overtime)
     {
 	this.name = name;
-	this.role = role;
+	this.role = "Engineer";
 	this.address = address;
 	this.gender = gender;
 	this.basicSalary = basicSalary;
@@ -76,11 +84,10 @@ class SalesPerson extends Employee
 {
     int commission;
 
-    public SalesPerson(String name, String role, String address, String gender, double basicSalary, int age,
-	    int commission)
+    public SalesPerson(String name, String address, String gender, double basicSalary, int age, int commission)
     {
 	this.name = name;
-	this.role = role;
+	this.role = "SalesPerson";
 	this.address = address;
 	this.gender = gender;
 	this.basicSalary = basicSalary;
@@ -91,14 +98,16 @@ class SalesPerson extends Employee
 }
 
 // ---------------- Employee --------------------
-public class Employee
+public class Employee implements Serializable
 {
     String name, address, gender, role;
     double basicSalary;
     int age;
+    static int i = 0;
 
     // -------------------------MenuDisplay-------------------------------------------------
-    public static String menuDisplay()
+    @SuppressWarnings("unchecked")
+    public static String menuDisplay(LinkedList<Employee> employeeList) throws IOException, ClassNotFoundException
     {
 	{
 	    System.out.println("1. Add an Employee");
@@ -109,8 +118,6 @@ public class Employee
 	    System.out.println("6. Exit");
 	}
 	String menuSelected = ConsoleInput.getString();
-
-	String result;
 
 	switch (menuSelected)
 	{
@@ -125,22 +132,22 @@ public class Employee
 		}
 		String subMenu1Selected = ConsoleInput.getString();
 
-		result = switch (subMenu1Selected)
+		switch (subMenu1Selected)
 		{
-		case "a" -> "1a"; // add manager
+		case "a" -> employeeList.add(Employee.setManger()); // add manager
 
-		case "b" -> "1b"; // b add Engineer
+		case "b" -> employeeList.add(Employee.setEngineer()); // b add Engineer
 
-		case "c" -> "1c"; // c add sales person
+		case "c" -> employeeList.add(Employee.setSalesPerson()); // c add sales person
 
-		case "d" -> menuDisplay();
+		case "d" -> menuDisplay(employeeList);
 
 		default -> throw new IllegalArgumentException("Unexpected value: " + menuSelected);
 
-		};
-		return result;
-	    }
+		}
 
+	    }
+	    break;
 	case "2":
 	    {
 		{
@@ -154,19 +161,39 @@ public class Employee
 		}
 		String subMenu2Selected = ConsoleInput.getString();
 
-		result = switch (subMenu2Selected)
+		switch (subMenu2Selected)
 		{
-		case "a" -> "2a";
-		case "b" -> "2b";
-		case "c" -> "2c";
-		case "d" -> "2d";
-		case "e" -> "2e";
-		case "f" -> menuDisplay();
-		default -> throw new IllegalArgumentException("Unexpected value: " + subMenu2Selected);
-		};
-		return result;
-	    }
+		case "a" -> Employee.displayAllEmployees(employeeList);
+		case "b" -> Employee.displayEmployee(employeeList.getFirst());
+		case "c" ->
+		    {
 
+			if (i == employeeList.size() - 1)
+			{
+			    i = employeeList.size() - 1;
+			    break;
+			}
+			Employee.displayEmployee(employeeList.get(i + 1));
+			i++;
+		    }
+		case "d" ->
+		    {
+			if (i < -1)
+			{
+			    i = 0;
+			    break;
+			}
+			Employee.displayEmployee(employeeList.get(i - 1));
+			i++;
+		    }
+		case "e" -> Employee.displayEmployee(employeeList.getLast());
+		case "f" -> menuDisplay(employeeList);
+		default -> throw new IllegalArgumentException("Unexpected value: " + subMenu2Selected);
+		}
+		;
+
+	    }
+	    break;
 	case "3":
 	    {
 		{
@@ -182,40 +209,89 @@ public class Employee
 
 		switch (subMenu3Selected)
 		{
-		case "a":
+		case "a" -> // sort all managers
 		    {
+			LinkedList<Employee> e = new LinkedList<>();
+			;
+			for (Employee employee : employeeList)
+			{
+			    if (employee.role == "Manager")
+				e.add(employee);
+			}
+			e.sort(Comparator.comparing(Employee::getName));
+			for (Employee employee : e)
+			{
+			    displayEmployee(employee);
+			}
 		    }
-		    break;
-		case "b":
+		case "b" -> // sort all Engineers
 		    {
+			LinkedList<Employee> e = new LinkedList<>();
+			;
+			for (Employee employee : employeeList)
+			{
+			    if (employee.role == "Engineer")
+				e.add(employee);
+			}
+			e.sort(Comparator.comparing(Employee::getName));
+			for (Employee employee : e)
+			{
+			    displayEmployee(employee);
+			}
 		    }
-		    break;
-		case "c":
+		case "c" -> // sort all sales Person
 		    {
+			LinkedList<Employee> e = new LinkedList<>();
+			;
+			for (Employee employee : employeeList)
+			{
+			    if (employee.role == "SalesPerson")
+				e.add(employee);
+			}
+			e.sort(Comparator.comparing(Employee::getName));
+			for (Employee employee : e)
+			{
+			    displayEmployee(employee);
+			}
 		    }
-		    break;
-		case "d":
+		case "d" -> // sorting all in alphabetical order
 		    {
+			employeeList.sort(Comparator.comparing(Employee::getName));
+			displayAllEmployees(employeeList);
 		    }
-		    break;
-		case "e":
+		case "e" -> // sorting all in reverse alphabetical order
 		    {
+			employeeList.sort(Comparator.comparing(Employee::getName).reversed());
+			displayAllEmployees(employeeList);
 		    }
-		    break;
-		case "f":
-		    menuDisplay();
-		    break;
-		default:
-		    throw new IllegalArgumentException("Unexpected value: " + menuSelected);
+		default -> throw new IllegalArgumentException("Unexpected value: " + subMenu3Selected);
 		}
 
 	    }
 	    break;
-	case "4":
-	    // save to file
+	case "4": // save in the file
+	    {
+		FileOutputStream fos = new FileOutputStream("employees.txt");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(employeeList);
+		oos.close();
+		fos.close();
+		System.out.println("File saved..");
+	    }
 	    break;
-	case "5":
-	    // load from file
+	case "5": // load from file
+	    {
+		FileInputStream fis = new FileInputStream("employees.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		
+		employeeList.clear();
+		employeeList.addAll( (LinkedList<Employee>) ois.readObject());
+		
+		System.out.println("File Loaded..");
+		System.out.println(employeeList.size()+" Employees loaded From File..");
+		ois.close();
+		fis.close();
+	    }
 	    break;
 	case "6":
 
@@ -223,10 +299,20 @@ public class Employee
 
 	default:
 	    System.out.println("Invalid input try again");
-	    menuDisplay();
+	    menuDisplay(employeeList);
 	}
 	return "";
 
+    }
+
+    public String getName()
+    {
+	return name;
+    }
+
+    public void setName(String name)
+    {
+	this.name = name;
     }
 
     // ------------------------------------getManager
@@ -250,7 +336,7 @@ public class Employee
 	System.out.println("Enter HRQ Score : ");
 	int hrqscore = ConsoleInput.getInt();
 
-	return new Manager(name, "Manager", address, gender, basicSalary, age, hrqscore);
+	return new Manager(name, address, gender, basicSalary, age, hrqscore);
     }
 
     // ------------------------------------getEngineer
@@ -274,7 +360,7 @@ public class Employee
 	System.out.println("Enter overtime : ");
 	int overtime = ConsoleInput.getInt();
 
-	return new Engineer(name, "Engineer", address, gender, basicSalary, age, overtime);
+	return new Engineer(name, address, gender, basicSalary, age, overtime);
     }
 
     // ------------------------------------getSalesPerson
@@ -298,99 +384,64 @@ public class Employee
 	System.out.println("Enter commission : ");
 	int commission = ConsoleInput.getInt();
 
-	return new SalesPerson(name, "SalesPerson", address, gender, basicSalary, age, commission);
+	return new SalesPerson(name, address, gender, basicSalary, age, commission);
     }
 
     public static void displayAllEmployees(LinkedList<Employee> employeeList)
     {
 	for (Employee employee : employeeList)
 	{
-	    if (employee instanceof Manager manager)
-		System.out.println("------------------\n\tRole : " + manager.role + "\nName : " + manager.name
-			+ "\nAddress : " + manager.address + "\nAge : " + manager.age + "\nGender :  " + manager.gender
-			+ "\nBasic salary : " + manager.basicSalary + "\nHRQ score : " + manager.hrqScore
-			+ "\n------------------");
-
-	    if (employee instanceof Engineer engineer)
-		System.out.println("------------------\nName : " + engineer.name + "\nAddress : " + engineer.address
-			+ "\nAge : " + engineer.age + "\nGender :  " + engineer.gender + "\nBasic salary : "
-			+ engineer.basicSalary + "\nOvertime : " + engineer.overtime + "\n------------------");
-	    if (employee instanceof SalesPerson salesPerson)
-		System.out.println("------------------\nName : " + salesPerson.name + "\nAddress : "
-			+ salesPerson.address + "\nAge : " + salesPerson.age + "\nGender :  " + salesPerson.gender
-			+ "\nBasic salary : " + salesPerson.basicSalary + "\nCommission : " + salesPerson.commission
-			+ "\n------------------");
+	    displayEmployee(employee);
 	}
     }
 
     public static void displayEmployee(Employee employee)
     {
-	 if (employee instanceof Manager manager)
-		System.out.println("------------------\n\tRole : " + manager.role + "\nName : " + manager.name
-			+ "\nAddress : " + manager.address + "\nAge : " + manager.age + "\nGender :  " + manager.gender
-			+ "\nBasic salary : " + manager.basicSalary + "\nHRQ score : " + manager.hrqScore
-			+ "\n------------------");
+	if (employee instanceof Manager manager)
+	    System.out.println("------------------\nRole : " + manager.role + "\nName : " + manager.name
+		    + "\nAddress : " + manager.address + "\nAge : " + manager.age + "\nGender :  " + manager.gender
+		    + "\nBasic salary : " + manager.basicSalary + "\nHRQ score : " + manager.hrqScore
+		    + "\n------------------");
 
-	    if (employee instanceof Engineer engineer)
-		System.out.println("------------------\nName : " + engineer.name + "\nAddress : " + engineer.address
-			+ "\nAge : " + engineer.age + "\nGender :  " + engineer.gender + "\nBasic salary : "
-			+ engineer.basicSalary + "\nOvertime : " + engineer.overtime + "\n------------------");
-	    if (employee instanceof SalesPerson salesPerson)
-		System.out.println("------------------\nName : " + salesPerson.name + "\nAddress : "
-			+ salesPerson.address + "\nAge : " + salesPerson.age + "\nGender :  " + salesPerson.gender
-			+ "\nBasic salary : " + salesPerson.basicSalary + "\nCommission : " + salesPerson.commission
-			+ "\n------------------");
+	if (employee instanceof Engineer engineer)
+	    System.out.println("------------------\nRole : " + engineer.role + "\nName : " + engineer.name
+		    + "\nAddress : " + engineer.address + "\nAge : " + engineer.age + "\nGender :  " + engineer.gender
+		    + "\nBasic salary : " + engineer.basicSalary + "\nOvertime : " + engineer.overtime
+		    + "\n------------------");
+	if (employee instanceof SalesPerson salesPerson)
+	    System.out.println("------------------\nRole : " + salesPerson.role + "\nName : " + salesPerson.name
+		    + "\nAddress : " + salesPerson.address + "\nAge : " + salesPerson.age + "\nGender :  "
+		    + salesPerson.gender + "\nBasic salary : " + salesPerson.basicSalary + "\nCommission : "
+		    + salesPerson.commission + "\n------------------");
     }
 
     // ---------------------------- M a i n -------------------------------------
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException, ClassNotFoundException
     {
 	LinkedList<Employee> employeeList = new LinkedList<Employee>();
-
-	String result;
 	
-	int i=0;
+	
+	{
+	    employeeList.add(new Manager("Rohan", "Mumbai", "Male", 12000, 23, 4));
+	    employeeList.add(new Engineer("Aryan", "Pune", "Male", 15000, 22, 3));
+	    employeeList.add(new SalesPerson("Tushar", "Mumbai", "Male", 22000, 26, 2));
+	    employeeList.add(new Manager("Karan", "Mumbai", "Male", 25000, 25, 2));
+	    employeeList.add(new SalesPerson("Raju", "Delhi", "Male", 22000, 26, 4));
+	    employeeList.add(new Manager("Priti", "Pune", "Female", 52000, 23, 5));
+	}
+	String result;
+
 	do
 	{
-
-	    result = Employee.menuDisplay();
+	    result = Employee.menuDisplay(employeeList);
 	    switch (result)
 	    {
 	    case "exit" -> System.out.println("exited...");
-
-	    case "1a" -> // add manager
-		{
-		    employeeList.add(Employee.setManger());
-		}
-	    case "1b" -> // add engineer
-		{
-		    employeeList.add(Employee.setEngineer());
-		}
-	    case "1c" -> // add sales guy
-		{
-		    employeeList.add(Employee.setSalesPerson());
-		}
-	    case "2a" -> // Display All
-		{
-		    Employee.displayAllEmployees(employeeList);
-		}
-	    case "2b" -> // Display first
-		{
-		    Employee.displayEmployee(employeeList.getFirst());
-		}
-	    case "2c" -> // Display next
-		{
-		    Employee.displayEmployee(employeeList.get(0+i)); 
-		    i++;
-		}
-
-	    default -> System.out.println("invalid option");
-
+	    default -> System.out.println(employeeList.size()+" employees loaded...");
 	    }
-	} while (result != "exit");
+	} while (!result.equals("exit"));
 
     }
 
-   
 }
